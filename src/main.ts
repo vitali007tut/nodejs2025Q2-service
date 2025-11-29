12e6d2987e792c05b9f2e2357216230646cf88fe
Rydkin Vitali <vrydkin@ibagroup.eu>
2025-11-29 18:28:09 +0300

    chore: comment out global prefix setting in main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = parseInt(process.env.PORT || '4000', 10);
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
